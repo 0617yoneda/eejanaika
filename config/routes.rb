f735365c4e80c576cc770de5557b8e46b7c62a22
0617yoneda <0617yoneda@gmail.com>
2021-01-05 11:54:17 +0000

    [add]管理者側ルーティング設定済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+
   devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
 }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admins do
+    resources :customers, only: [:index, :edit, :update]
+    resources :posts, only: [:index, :edit, :update]
+    resources :post_comments, only: [:destroy]
+  end
 end
