2beffaa6fa73f929b2307f85518f229788e52e61
0617yoneda <0617yoneda@gmail.com>
2021-01-06 14:02:00 +0000

    [add]設計書通りのmodel追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :customer
   belongs_to :category
-
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   attachment :image
 end
