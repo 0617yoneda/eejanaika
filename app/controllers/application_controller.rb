d7266e01a26e697b04b49c3b7d94909ddda03021
0617yoneda <0617yoneda@gmail.com>
2021-01-06 12:11:55 +0000

    [add]ヘッダー、フッター文言追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    if customer_signed_in?
+      posts_path(resource)
+    else
+      admins_posts_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+      root_path
+    else
+      root_path
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       :nickname,
