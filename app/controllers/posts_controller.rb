8de221bcf28cb7722ed05ae23dceae998528ab19
0617yoneda <0617yoneda@gmail.com>
2021-01-06 07:33:44 +0000

    [add]新規登録画面実装済み

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.customer_id = current_customer.id
+    @post.save
+    redirect_to posts_path
   end
 
   def index
@@ -17,3 +22,9 @@ class PostsController < ApplicationController
   def update
   end
 end
+
+private
+
+  def post_params
+    params.require(:post).permit(:category_id, :title, :body, :try, :image)
+  end
