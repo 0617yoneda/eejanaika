fb681d521e1bb9fb48ff119a852ba695d6e8b911
0617yoneda <0617yoneda@gmail.com>
2021-01-07 07:43:02 +0000

    [add]投稿詳細画面にコメント投稿機能実装済み

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,22 @@
 class PostCommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.customer_id = current_customer.id
+    @post_comment.post_id = @post.id
+    @post_comment.save
+    redirect_to post_path(@post)
   end
 
   def destroy
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_post_comment_path(params[:post_id])
   end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+
 end
