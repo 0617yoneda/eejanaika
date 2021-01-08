9461d19e0669c07f9759e07194f9786aa9db8f87
0617yoneda <0617yoneda@gmail.com>
2021-01-08 17:31:35 +0000

    [add]管理者側　会員編集画面実装完了

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,10 +1,21 @@
 class Admins::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:profile_image, :crazy, :word)
+  end
+
 end
