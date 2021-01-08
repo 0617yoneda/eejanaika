84faefd0de45e9ab173cf869b5e8b7b9c0a3c577
0617yoneda <0617yoneda@gmail.com>
2021-01-08 14:58:21 +0000

    [add]プロフィール画面CRUD処理実装済み

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,14 +5,24 @@ class CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to posts_path
   end
 
   def out
   end
 
   def hide
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:nickname, :profile_image, :crazy, :word)
   end
 end
