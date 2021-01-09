86d78437f009be5bd4f2a7620142f0381f977471
0617yoneda <0617yoneda@gmail.com>
2021-01-09 15:24:54 +0000

    [add]通知一覧画面　プロフィール追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_customer!
   def index
+      @customer = current_customer
       @notifications = current_customer.passive_notifications
       @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
